7b54e64502371b6a69a345cecde1acd0c5240860
shuvo-me <shuvonaim123@gmail.com>
2022-08-12 00:50:34 +0600

    added a simple preloader component

---
+++ components/Preloader.jsx
@@ -0,0 +1,25 @@
+const Preloader = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "#000000",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h2
+        style={{
+          color: "#FDDF48",
+          fontSize: "40px",
+          marginTop: "200px",
+        }}
+      >
+        TASKY.........
+      </h2>
+    </div>
+  );
+};
+
+export default Preloader;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
+import Preloader from "@/components/Preloader";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import store from "store/store";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </Provider>
   );
 }
